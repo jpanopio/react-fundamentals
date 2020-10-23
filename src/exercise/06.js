da752663d5c7a06cb9f34195a0b6d648a12ce837
Julien Panopio <jcpanopio1@gmail.com>
2020-10-23 11:08:03 -0700

    Do Form exercises

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -1,7 +1,7 @@
 // Basic Forms
 // http://localhost:3000/isolated/exercise/06.js
 
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 function UsernameForm({onSubmitUsername}) {
   // 🐨 add a submit event handler here (`handleSubmit`).
@@ -18,13 +18,54 @@ function UsernameForm({onSubmitUsername}) {
 
   // 🐨 make sure to associate the label to the input by specifying an `id` on
   // the input and a matching value as an `htmlFor` prop on the label.
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const username = event.target.elements[0].value;
+    onSubmitUsername(username);
+  };
+
+  const inputRef = useRef(null);
+  const handleSubmitWithRef = (event) => {
+    event.preventDefault();
+    const username = inputRef.current.value;
+    onSubmitUsername(username);
+  };
+
+  const [errorMessage, setSErrorMessage] = useState(null);
+  const handleChange = (event) => {
+    const currentValue = event.target.value;
+    const isValid = currentValue === currentValue.toLowerCase();
+
+    if (!isValid) {
+      setSErrorMessage('Username must be lower case');
+    } else {
+      setSErrorMessage(null);
+    }
+  };
+
+  const [inputValue, setInputValue] = useState('');
+  const handleChangeControlled = (event) => {
+    event.preventDefault();
+    const currentValue = inputRef.current.value;
+    setInputValue(currentValue.toLowerCase());
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmitWithRef}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="username-input">Username:</label>
+        <input
+          ref={inputRef}
+          value={inputValue}
+          onChange={handleChangeControlled}
+          id="username-input"
+          type="text"
+        />
       </div>
       <button type="submit">Submit</button>
+      {errorMessage && (
+        <span>{errorMessage}</span>
+      )}
     </form>
   )
 }
